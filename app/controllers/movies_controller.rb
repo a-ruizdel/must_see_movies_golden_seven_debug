3e8619e4914520ca91ff1105da5b6ad77f863966
a-ruizdel <a-rdl@wireless-s1-no-150-23-151.uchicago.edu>
2015-08-05 12:40:31 -0500

    fixes create row and new form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,19 +9,20 @@ class MoviesController < ApplicationController
   end
 
   def new_form
+    render("movies/new_form.html.erb")
   end
 
   def create_row
     @movie = Movie.new
-    @movie.title = params[:the_title]
-    @movie.year = params[:the_year]
-    @movie.duration = params[:the_duration]
-    @movie.description = params[:the_description]
-    @movie.image_url = params[:the_image_url]
-    @movie.director_id = params[:the_director_id]
+    @movie.title = params["title"]
+    @movie.year = params["year"]
+    @movie.duration = params["duration"]
+    @movie.description = params["description"]
+    @movie.image_url = params["image_url"]
+    @movie.director_id = params["director_id"]
     @movie.save
 
-    redirect_to("/movies/@movie.id")
+    render("/movies/show.html.erb")
   end
 
   def edit_form
