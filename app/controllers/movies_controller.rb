1b69a84758f731f0b8db33c562b90eb27b667f3d
a-ruizdel <a-rdl@wireless-s1-no-150-23-151.uchicago.edu>
2015-08-05 12:30:50 -0500

    fixes the edit_row method and rendering

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,31 +12,34 @@ class MoviesController < ApplicationController
   end
 
   def create_row
-    m = Movie.new
-    m.movie_title = params["the_title"]
-    m.movie_year = params["the_year"]
-    m.movie_duration = params["the_duration"]
-    m.movie_description = params["the_description"]
-    m.movie_image_url = params["the_image_url"]
-    m.movie_director_id = params["the_director_id"]
-    m.save
+    @movie = Movie.new
+    @movie.title = params[:the_title]
+    @movie.year = params[:the_year]
+    @movie.duration = params[:the_duration]
+    @movie.description = params[:the_description]
+    @movie.image_url = params[:the_image_url]
+    @movie.director_id = params[:the_director_id]
+    @movie.save
 
-    render("/movies/show.html.erb")
+    redirect_to("/movies/@movie.id")
   end
 
   def edit_form
-    @movie = Movie.find(params[:id])
+    @movie = Movie.find(params["id"])
+
   end
 
   def update_row
-    @movie.title = params[:title]
-    @movie.year = params[:year]
-    @movie.duration = params[:duration]
-    @movie.description = params[:description]
-    @movie.image_url = params[:image_url]
-    @movie.director_id = params[:director_id]
+    @movie = Movie.find(params["id"])
+    @movie.title = params["title"]
+    @movie.year = params["year"]
+    @movie.duration = params["duration"]
+    @movie.description = params["description"]
+    @movie.image_url = params["image_url"]
+    @movie.director_id = params["director_id"]
+    @movie.save
 
-    render("show")
+    render("/movies/show.html.erb")
   end
 
   def destroy
